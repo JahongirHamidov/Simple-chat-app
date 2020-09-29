6c29410f9ca30f481b96a94079241675448319e1
Jahongir <hamidovjahongir444@gmail.com>
2020-09-29 18:32:47 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,63 +1,26 @@
 import React from 'react'
 import './App.css'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import { MyAppbar } from './Components/MyAppbar/MyAppbar'
-import { Content } from './Components/Content/Content'
 import {Provider} from './globalState'
+import { Dashboard } from './Components/Dashboard/Dashboard'
+import  Container  from '@material-ui/core/Container' 
 
-import {Trending} from './Components/Trending/Trending'
 
 
 function App() {
   return (
     <Provider>
     <BrowserRouter>
-      <div className="App">
-        <MyAppbar/>
-        <Switch>
-        {/* <iframe 
-          width="420" 
-          height="345" 
-          src="http://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1" 
-          frameborder="0" 
-          allowfullscreen>
-            
-          </iframe> */}
-            <Route path='/' exact>
-              <div className='home'>
-                <Content/>
-                <Content/>
-              </div>
-            </Route>
-            <Route path='/checkout'>
-
-            </Route>
-            <Route path='/login'>
-              <p>Login page</p>
-            </Route>
-            <Route path='/trending'>
-              <Trending />
-            </Route>
-            <Route path='/subscription'>
-
-            </Route>
-            <Route path='/library'>
-
-            </Route>
-            <Route path='/history'>
-
-            </Route>
-            <Route path='/yourVideos'>
-
-            </Route>
-            <Route path='watchLater'>
-
-            </Route>
-            <Route path='/likedVideos'>
-
-            </Route>
-        </Switch>
-      </div>
+      <Container fixed className='container'>
+        <div className="App">
+          <Switch>
+              <Route path='/' exact>
+                <Dashboard />
+                
+              </Route>
+          </Switch>
+        </div>
+      </Container>
     </BrowserRouter>
     </Provider>
   )
---
+++ src/Components/Dashboard/useDashboardStyles.js
@@ -0,0 +1,7 @@
+import {makeStyles} from '@material-ui/styles'
+
+export const useDashboardStyles = makeStyles((theme)=>({
+    paper: {
+
+    },
+}))
